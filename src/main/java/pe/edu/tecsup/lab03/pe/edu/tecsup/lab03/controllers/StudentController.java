a77bac046174c3ed22a467d3a5af23adc98debc3
Jefferson <jefferson.bautista@tecsup.edu.pe>
2025-09-04 17:31:40 -0500

    metodo destroy

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,20 +1,18 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-        void init() {
-            System.out.println("Hola");
-        }
 
-        public void execute(){
-            System.out.println("Buenas Noches");
-        }
-
-        public static void main(String[] args) {
-            StudentController stuc = new StudentController();
-            stuc.init();
-            stuc.execute();
+    public void init() {
+        System.out.println("Hola");
     }
 
+    public void execute() {
+        System.out.println("Buenas Noches");
+    }
+
+    public static void main(String[] args) {
+        StudentController stuc = new StudentController();
+        stuc.init();
+        stuc.execute();
+    }
 }
-
-
