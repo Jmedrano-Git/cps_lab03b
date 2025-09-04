d1ec4c9a28d23c2cf35949069bfae0061c0b8178
Jefferson <jefferson.bautista@tecsup.edu.pe>
2025-09-04 17:35:15 -0500

    metodo destroy correguido

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,18 +1,24 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+        void init() {
+            System.out.println("Hola");
+        }
 
-    public void init() {
-        System.out.println("Hola");
+        public void execute(){
+            System.out.println("Buenas Noches");
+        }
+        public void destroy() {
+            System.out.println("Componente destruido");
+        }
+
+        public static void main(String[] args) {
+            StudentController stuc = new StudentController();
+            stuc.init();
+            stuc.execute();
+            stuc.destroy();
     }
 
-    public void execute() {
-        System.out.println("Buenas Noches");
-    }
-
-    public static void main(String[] args) {
-        StudentController stuc = new StudentController();
-        stuc.init();
-        stuc.execute();
-    }
 }
+
+
