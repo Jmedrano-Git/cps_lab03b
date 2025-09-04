bb5f37b8a3772028ee1e6e2b4e7972904d08595f
Cvaleriano <carlos@tecsup.com>
2025-09-04 17:17:13 -0500

    metodo init()

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,14 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-}
+
+        void init() {
+            System.out.println("Hola");
+        }
+
+        public static void main(String[] args) {
+            new StudentController().init();
+        }
+    }
+
+
