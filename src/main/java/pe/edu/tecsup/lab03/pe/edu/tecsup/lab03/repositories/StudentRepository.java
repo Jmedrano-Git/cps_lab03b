a5f8f7e80b581fa24a1a0ff4e6fbcb92daa9c30c
Cvaleriano <carlos@tecsup.com>
2025-09-04 17:16:21 -0500

    metodo init()

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,13 +1,7 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
-        void init() {
-            System.out.println("Hola");
-        }
 
-        public static void main(String[] args) {
-            new StudentRepository().init();
-        }
     }
 
 
